ed18d432d325aab1e2d09e2a324e9daada641dc3
Irarupa <62602926+Irarupa@users.noreply.github.com>
2026-07-27 07:40:48 +0530

    Add: is-palindrome - submission-0

---
+++ Data Structures & Algorithms/is-palindrome/submission-0.py
@@ -0,0 +1,6 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        text = "".join(char.lower() for char in s if char.isalnum())
+        res = text[::-1]
+
+        return text == res
